8dcdcf1ea61dcfcbeec3040d38183743c04fb38c
JactusTheCactus <threateninglyamusing@gmail.com>
2025-06-16 23:14:19 -0700

    Update script.js

--- script.js
+++ script.js
@@ -12,8 +12,7 @@ function convertToPug(str) {
   const INDENT = '  ';
 
   const lines = str
-.replace(/\n/g,"")
-.replace(/\t| {2,}/g,"")
+.replace(/\n|\t|(?: {2,})/g,"")
     .replace(/;/g, '')// remove semicolons
     .replace(/\{/g, ' {\n')// ensure braces open on new lines
     .replace(/\}/g, '\n}')// ensure braces close on new lines
